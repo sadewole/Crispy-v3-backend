18378accee072a46e131822976956e8c30e7e243
sadewole <samueladewole15@gmail.com>
2021-05-19 20:08:46 +0100

    clean sanitalizer

--- middlewares/sanitizer.js
+++ middlewares/sanitizer.js
@@ -7,7 +7,6 @@ module.exports = {
       check('password', 'Please enter a password').not().isEmpty(),
       check('firstName', 'Please enter first name').not().isEmpty(),
       check('lastName', 'Please enter last name').not().isEmpty(),
-      check('role', 'Please enter role').not().isEmpty(),
     ],
     signin: [
       check('email', 'Please enter a valid email').isEmail(),
